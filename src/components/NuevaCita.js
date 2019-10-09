23a99265cb31c6cec084e2e1d9ca4ff84c9338de
larrygf02 <raulgf_02@hotmail.com>
2019-10-08 20:40:44 -0500

    Mostrando mensaje de error

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -41,12 +41,16 @@ class NuevaCita extends Component {
         this.props.crearNuevaCita(nuevaCita)
     }
     render() {
+        const { error } = this.state;
         return (
             <div className="card mt-5 py-5">
                 <div className="card-body">
                     <h2 className="card-title text-center mb-5">
                         Llena el formulario para crear una nueva cita
                     </h2>
+                    { error ? <div className="alert alert-danger mt-2 mb-5 text-center">
+                        Todos los campos son obligatorios
+                    </div> : null}
                     <form onSubmit={this.handleSubmit}>
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
