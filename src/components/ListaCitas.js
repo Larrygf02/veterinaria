34a443aadf3ce49b678b950f96f1ae1f09aed715
larrygf02 <raulgf_02@hotmail.com>
2019-10-09 22:21:05 -0500

    Mostrando mensaje si no hay citas

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -2,23 +2,27 @@ import React from 'react';
 import Cita from './Cita';
 import PropTypes from 'prop-types';
 
-const ListaCitas = ({citas, eliminarCita}) => (
-    <div className="card mt-2 py-5">
-        <div className="card-body">
-            <h2 className="card title text-center">
-                Administra las citas aquí
-            </h2>
-            <div className="lista-citas">
-                {citas.map(cita => (
-                    <Cita 
-                    key={cita.id}
-                    cita={cita}
-                    eliminarCita={eliminarCita}/>
-                ))}
+const ListaCitas = ({citas, eliminarCita}) => {
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aquí'
+    return (
+        <div className="card mt-2 py-5">
+            <div className="card-body">
+                <h2 className="card title text-center">
+                    { mensaje }
+                </h2>
+                <div className="lista-citas">
+                    {citas.map(cita => (
+                        <Cita 
+                        key={cita.id}
+                        cita={cita}
+                        eliminarCita={eliminarCita}/>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
+
 
 ListaCitas.propTypes = {
     citas: PropTypes.array.isRequired,
